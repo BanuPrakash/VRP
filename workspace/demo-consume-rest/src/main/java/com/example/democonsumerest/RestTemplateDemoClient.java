81028afe143b548b5ebd68822346c2d0e1037112
Banuprakash <banuprakashc@yahoo.co.in>
2024-10-08 12:25:42 +0530

    security

--- workspace/demo-consume-rest/src/main/java/com/example/democonsumerest/RestTemplateDemoClient.java
+++ workspace/demo-consume-rest/src/main/java/com/example/democonsumerest/RestTemplateDemoClient.java
@@ -3,11 +3,14 @@ package com.example.democonsumerest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -17,10 +20,26 @@ public class RestTemplateDemoClient implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 //        getPets();
-        getPet();
+    //    getPet();
+        getAllUsers();
+    }
+    record Owner(String firstName, String city){}
+    record PetDTO(String name, Date birthDate, Owner owner) {}
+
+    record  User(int id, String name, String email) {}
+    private void getAllUsers() {
+        ResponseEntity<List<User>> response = template
+                .exchange("https://jsonplaceholder.typicode.com/users", HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<User>>() {} );
+
+        List<User> users = response.getBody();
+
+        for(User user : users) {
+            System.out.println(user);
+        }
     }
 
-    record PetDTO(String name, Date birthDate) {}
+
 
     private void getPet() {
         ResponseEntity<PetDTO> petDTOResponse = template.getForEntity("http://localhost:1234/pets/2", PetDTO.class);
